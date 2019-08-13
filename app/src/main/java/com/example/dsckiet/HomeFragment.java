118a13b538270a97d20fb34a72c58a5138e6a4d7
shubham100ev <shubhamsingh19981110@gmail.com>
2019-08-13 23:25:27 +0530

    commit-5

--- app/src/main/java/com/example/dsckiet/HomeFragment.java
+++ app/src/main/java/com/example/dsckiet/HomeFragment.java
@@ -13,7 +13,6 @@ import android.view.ViewGroup;
 
 public class HomeFragment extends Fragment {
 
-    FragmentManager fm = getFragmentManager();
 
     public HomeFragment() {
     }
--- app/src/main/java/com/example/dsckiet/UpcomingEventsFragment.java
+++ app/src/main/java/com/example/dsckiet/UpcomingEventsFragment.java
@@ -9,14 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link UpcomingEventsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link UpcomingEventsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class UpcomingEventsFragment extends Fragment {
 
     public UpcomingEventsFragment() {
